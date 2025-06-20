cd7646f86053384fb3d4cf9ab16184c551d41b93
Bruno Souza <bbquirino@gmail.com>
2025-06-20 16:15:28 -0700

    Fix: add alias support for '@/lib/*' using tsconfig and webpack

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
-  }
-}
+const path = require('path');
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'app');
+    return config;
+  }
+};
+
+module.exports = nextConfig;
